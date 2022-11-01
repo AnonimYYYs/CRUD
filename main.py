4303003887fac440bc39fcfe9c0cf3fd8a7a661d
niknl <niknlo007@gmail.com>
2022-11-01 21:47:47 +0300

    фывфыв

--- main.py
+++ main.py
@@ -10,5 +10,29 @@ def main_page():
     return render_template('main.html', data=data)
 
 
+@app.route('/delete')
+def delete_page():
+    data = return_dull_db_select()  # todo()
+    return render_template('delete.html', data=data)
+
+
+@app.route('/select')
+def select_page():
+    data = return_dull_db_select()  # todo()
+    return render_template('select.html', data=data)
+
+
+@app.route('/update')
+def update_page():
+    data = return_dull_db_select()  # todo()
+    return render_template('update.html', data=data)
+
+
+@app.route('/insert')
+def insert_page():
+    data = return_dull_db_select()  # todo()
+    return render_template('insert.html', data=data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
